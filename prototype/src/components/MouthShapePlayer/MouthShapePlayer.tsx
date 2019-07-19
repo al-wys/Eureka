cc14b84dd9c89ec0513994e8e66b20552c06fbc6
Frank Yishuai Wang <yiswang@microsoft.com>
2019-07-19 14:44:39 +0800

    Set default width, height

--- prototype/src/components/MouthShapePlayer/MouthShapePlayer.tsx
+++ prototype/src/components/MouthShapePlayer/MouthShapePlayer.tsx
@@ -5,7 +5,7 @@ import { IMouthShapePlayerProps } from './IMouthShapePlayer.types';
 export class MouthShapePlayer extends React.Component<IMouthShapePlayerProps> {
     render() {
         return (
-            <video autoPlay={false} controls={true} height={this.props.height} width={this.props.width} className={this.props.className}>
+            <video autoPlay={false} controls={true} height={this.props.height || "auto"} width={this.props.width || "auto"} className={this.props.className}>
                 <source src={this.props.src} type={this.props.type} />
             </video>
         );
